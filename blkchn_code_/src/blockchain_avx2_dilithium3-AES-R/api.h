a915734766396f4a7c4e7825e3d7cc6aa844c328
Baptoo10 <baptiste.cpry@gmail.com>
2024-02-22 16:53:15 +0100

    first commit

---
+++ blkchn_code_/src/blockchain_avx2_dilithium3-AES-R/api.h
@@ -0,0 +1,25 @@
+#ifndef API_H
+#define API_H
+
+#include "config.h"
+
+#if DILITHIUM_MODE == 3
+#define CRYPTO_PUBLICKEYBYTES 1952
+#define CRYPTO_SECRETKEYBYTES 4016
+#define CRYPTO_BYTES 3293
+#endif
+
+#define crypto_sign_keypair DILITHIUM_NAMESPACE(_keypair)
+int crypto_sign_keypair(unsigned char *pk, unsigned char *sk);
+
+#define crypto_sign DILITHIUM_NAMESPACE()
+int crypto_sign(unsigned char *sm, unsigned long long *smlen,
+                const unsigned char *msg, unsigned long long len,
+                const unsigned char *sk);
+
+#define crypto_sign_open DILITHIUM_NAMESPACE(_open)
+int crypto_sign_open(unsigned char *m, unsigned long long *mlen,
+                     const unsigned char *sm, unsigned long long smlen,
+                     const unsigned char *pk);
+
+#endif
